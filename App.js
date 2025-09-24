f297fa019c6aa9abbf11c94e0fab4e48461e5a28
eron-jay <delatorreaaronjay91@gmail.com>
2025-09-25 00:50:01 +0800

    midact1

--- App.js
+++ App.js
@@ -1,18 +1,12 @@
+import React from "react";
+import ChatScreen from "./ChatScreen";
+import CommentSection from "./CommentSection";
 
-// App.js
-import React from 'react';
-import { SafeAreaView } from 'react-native';
-import CounterApp from './CounterApp';
-import ColorChangerApp from './ColorChangerApp';
-
-const App = () => {
-    return (
-        <SafeAreaView style={{ flex: 1 }}>
-            <CounterApp />
-            <ColorChangerApp />
-        </SafeAreaView>
-    );
-};
-
-export default App;
-
+export default function App() {
+  return (
+    <>
+      <ChatScreen />
+      <CommentSection />
+    </>
+  );
+}
